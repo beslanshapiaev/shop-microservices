5027a3b202251c82683f8751bb32ed957c1cc082
mishastalinaa <mishastalinaa@yandex.ru>
2023-12-30 02:29:23 +0300

    Discount.API done

--- src/Services/Discount/Discount.API/Program.cs
+++ src/Services/Discount/Discount.API/Program.cs
@@ -1,14 +1,18 @@
+using Discount.API.Repositories;
+using Discount.API.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
+// Add services to the container.
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
 
 var app = builder.Build();
-
+app.Services.MigrateDatabase<Program>(0);
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
